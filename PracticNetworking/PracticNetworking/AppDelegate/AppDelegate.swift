5cd2d85750d68560d83167f95ecd8598807ff624
Sergey Gnid <serhii.hnid@gmail.com>
2021-10-22 03:37:59 +0300

    Some mark

--- PracticNetworking/PracticNetworking/AppDelegate/AppDelegate.swift
+++ PracticNetworking/PracticNetworking/AppDelegate/AppDelegate.swift
@@ -9,13 +9,9 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-    
     var backgroundCompletionHandler: (() -> Void)?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 }
